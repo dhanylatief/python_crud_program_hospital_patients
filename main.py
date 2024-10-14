bc36342c65aa989b9aa82263e17d8ef63f1816d5
Dhany <m.dhany.latief@gmail.com>
2024-10-14 19:40:40 +0700

    Defining data and applying crud menu

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 # ===================================
-# [Your Program Title]
+# [Ophthalmology Patient Database]
 # ===================================
-# Developed by. [Enter your name]
-# JCDS - [Class Batch]
+# Developed by. [Muhammad Dhany Latief]
+# JCDS - [JCDS 0412]
 
-
+from utils import menu #, create, read, update, delete
 # /************************************/
 
 # /===== Data Model =====/
 # Create your data model here
-data = [] # Example data model
+patient_data = {"Name":['Edi','Fatkul','Darmawan','Ibnu','Arif'],
+                 "Waiting Time":[534, 555, 294, 159, 508],
+                 "Care Time":[645, 641, 579, 517, 510],
+                 "Exam Time": [111, 86, 284, 358, 101], 
+                 "Subspecialty": ['Rekonstruksi','Rekonstruksi','Retina','Neuro Ophthalmology', 'Glaukoma'],
+                 "Insurance": ['BPJS','BPJS','Mandiri','BPJS','BPJS']} # Example data model
 
 
 # /===== Feature Program =====/
 # Create your feature program here
-def read():
+def read(patient_data: dict):
     """Function for read the data
     """
     return
@@ -39,7 +44,7 @@ def delete():
 def main():
     """Function for main program
     """
-
+    menu.crud_menu()
     input_user = input("Insert your option: ")
     if input_user == "1":
         read()
@@ -49,8 +54,10 @@ def main():
         update()
     elif input_user == "4":
         delete()
+    elif input_user == "5":
+        print("Exiting database...")
     else:
-        print("Input is not valid !")
+        print("Input invalid !")
 
 
 if __name__ == "__main__":
---
+++ utils/menu.py
@@ -0,0 +1,10 @@
+def crud_menu():
+    return print('''Welcome to Ophthalmology Patient Database
+1. View Patient Database
+2. Add a New Patient
+3. Edit a Patient Data
+4. Delete a Patient Data
+5. Exit the Program
+''')
+    
+# crud_menu() -> use this command to call the crud menu
